605d06616f657512598223ac4a1019c74fa4bff7
cogv <clecio.silva@gft.com>
2023-12-13 01:34:47 -0300

    Commit 1

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria 
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular, double saldoInicial = 0)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldoInicial;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor + 3.50; // Taxa de saque de $3.50
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2")}";
+        }
+    }
+}
---
+++ Questao2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        await GetGoalsScored("Paris Saint-Germain", 2013);
+        await GetGoalsScored("Chelsea", 2014);
+    }
+
+    static async Task GetGoalsScored(string teamName, int year)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            var apiUrl = $"https://jsonmock.hackerrank.com/api/football_matches?team1={teamName}&year={year}";
+            var response = await httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var jsonObject = JObject.Parse(jsonString);
+
+                int totalGoals = 0;
+                foreach (var match in jsonObject["data"])
+                {
+                    totalGoals += (int)match["team1goals"];
+                }
+
+                Console.WriteLine($"Team {teamName} scored {totalGoals} goals in {year}");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to fetch data for {teamName} in {year}");
+            }
+        }
+    }
+}
